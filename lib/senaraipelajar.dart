5c56fabd742c4441901781e8818964e3e1b376a7
BB Caspian <casfian@gmail.com>
2021-08-18 12:36:07 +0800

    kasi gambar

--- lib/senaraipelajar.dart
+++ lib/senaraipelajar.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:lkpistdemo/pelajar.dart';
 
 class SenaraiPelajar extends StatelessWidget {
   //array
-  List pelajar = ['Ali', 'Ahmad', 'Abu', 'Basri'];
+  List<Pelajar> pelajar = [
+    Pelajar('Ali', 'images/ADAM.jpg', '4.0'),
+    Pelajar('Ahmad', 'images/AMIRUL.jpg', '3.8'),
+    Pelajar('Abu', 'images/HARIZ.jpg', '3.6'),
+    Pelajar('Basri', 'images/RAFIQ.jpg', '3.9')
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +19,22 @@ class SenaraiPelajar extends StatelessWidget {
       body: ListView.builder(
           itemCount: pelajar.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(pelajar[index]),
+            //return sebuat widget UI you buat sendiri
+            return Container(
+              margin: EdgeInsets.all(10),
+              height: 100,
+              child: Card(
+                child: Row(
+                  children: [
+                    Image.asset(pelajar[index].photo),
+                    Text(pelajar[index].cgpa, style: TextStyle(fontSize: 25),),
+                    SizedBox(width: 30,),
+                    Text(pelajar[index].nama, style: TextStyle(fontSize: 25, color: Colors.red),),
+                    SizedBox(width: 30,),
+                    
+                  ],
+                ),
+              ),
             );
           }),
     );
